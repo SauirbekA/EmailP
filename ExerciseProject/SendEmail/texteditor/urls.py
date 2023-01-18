159cb9bf55cec46d41858fde750f27f3f84332f4
SauirbekA <artikbaisauirbek@gmail.com>
2023-01-18 19:45:59 +0600

    ok

--- ExerciseProject/SendEmail/texteditor/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path,include
-from . import views
-urlpatterns = [
-    path('tedit/', views.index, name='index')
-]
--- ExerciseProject/SendEmail/texteditor/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Editor
-from .forms import EditorForm
-def index(request):
-    form = EditorForm()
-    return render(request, 'texteditor/index.html', {'form': form, 'title': 'Texteditor'})
